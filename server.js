7d85c619f5df45e28bfab8eee2660cbc670ecc0a
Osiris <potterosiris@gmail.com>
2020-07-02 21:35:53 -0400

    signup.js, signup.html

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 // express-session module thats allows you to get and set data
-const session = require("express-session");
-const passport = require("./config/passport");
-const db = require("./models");
-const routes = require("./routes");
+const session = require('express-session');
+const passport = require('./config/passport');
+const db = require('./models');
+const routes = require('./routes');
 
 const app = express();
 
@@ -11,15 +11,15 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }),
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/", routes);
+app.use('/', routes);
 
 // Sync sequelize models then start Express app
 // =============================================
