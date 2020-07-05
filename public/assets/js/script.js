8e34bce78aa5568eba1456000653fbea957947ae
dianaz <stingray2091@gmail.com>
2020-07-05 16:18:06 -0400

    linked the api with the search button

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,7 @@
 // api call for recipes instructions and ingredients =// 
 const apiKey = `2a60aadc108349cd8cf2e7ad1e59495a`
 
-const getRecipes = async () => {
-    const query = "burger";
+const getRecipes = async (query) => {/// get the lenth of this return. write a for each loop that will create cards 
     const res = await $.get(
         `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`
     );
@@ -21,15 +20,13 @@ const getIngredients = async id => {
     const res = await $.get(
         `https://api.spoonacular.com/recipes/${id}/ingredientWidget.json?apiKey=${apiKey}`
     );
-
     console.log(res);
 };
-
-getRecipes();
-
-
-
-
+$("#searchButton").on("click", function () {
+    const searchQuery = $(".form-control").val();
+    getRecipes(searchQuery);
+})
 
 
 
+// make a for loop for container length for the for loop would be for the recipes. get the length of rest 
