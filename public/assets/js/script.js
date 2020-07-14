f971fd67911db6dc898b460910a5981543d05b19
Mike V <mvee333@gmail.com>
2020-07-13 20:51:10 -0400

    changed saved.html, recipe.js, shoppinglist.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,54 +7,52 @@ $.post('/api/user_data').then((res) => {
   // sets user object to be used during search requests
   user = { id: res.id, email: res.email };
 });
-
 // function to dynamically create cards
-const createCards = (title, imageSrc, id, steps) => {
-  console.log(steps);
-  const cardEl = $('<div>', {
-    style: 'width: 18rem;',
-    class: 'card',
+const createCards = (title, imageSrc, id, description) => {
+  //   // console.log(steps);
+  const rowEl = $('<div>', {
+    class: 'row',
   });
-  const cardBodyEl = $('<div>', {
-    class: 'card-body',
+  const cardEl = $('<div>', {
+    class: 'card col-md-3 rounded',
   });
   const cardImgEl = $('<img>', {
-    class: 'card-img-top',
+    class: 'card-img result-images',
     src: imageSrc,
   });
-  const cardTitleEl = $('<h5>', {
-    class: 'card-title',
-  }).text(title);
-
-  const stepsList = $('<ol>');
-  steps.forEach((el) => {
-    const liEl = $('<li>').text(el.step);
-    stepsList.append(liEl);
-  });
-
   const saveBtnEl = $('<button>', {
-    class: 'btn btn-primary calendar-save',
+    class: 'btn btn-sm font-weight-bolder card-bottom save-recipe',
     'data-recipe-id': id,
     'data-recipe-title': title,
   }).text('Save to Calendar');
-  cardBodyEl.append(cardTitleEl, cardImgEl, stepsList, saveBtnEl);
-  cardEl.append(cardBodyEl);
-  $('.foodContainer').append(cardEl);
+  cardEl.append(cardImgEl, saveBtnEl);
+  const textWrapperEl = $('<div>', {
+    class: 'col-md-9 text-wrapper',
+  });
+  const titleEl = $('<h3>', {
+    class: 'recipe-title',
+  }).text(title);
+  const descriptionEl = $('<p>', {
+    class: 'description-text',
+  }).html(description);
+  textWrapperEl.append(titleEl, descriptionEl);
+  rowEl.append(cardEl, textWrapperEl);
+  $('.card-deck').append(rowEl);
 };
-
 // when clicked, will send query to back end and search for results
 $('#searchButton').on('click', () => {
   const searchQuery = $('.form-control').val();
   // getRecipes(searchQuery);
   $.get(`/api/recipes/search/${searchQuery}`).then((results) => {
+    console.log(results);
+    $('.card-deck').empty();
     results.forEach((result) => {
-      createCards(result.title, result.image, result.id, result.instructions[0].steps);
+      createCards(result.title, result.image, result.id, result.description);
     });
   });
 });
-
 // when clicked, will save the clicked recipe to the calendar and db
-$(document.body).on('click', '.calendar-save', (e) => {
+$(document.body).on('click', '.save-recipe', (e) => {
   // get recipe id and title from data attributes saved to the elements
   const id = e.target.getAttribute('data-recipe-id');
   const title = e.target.getAttribute('data-recipe-title');
@@ -71,6 +69,5 @@ $(document.body).on('click', '.calendar-save', (e) => {
       console.log(err);
     });
 });
-
 // make a for loop for container length for the
 // for loop would be for the recipes. get the length of rest
